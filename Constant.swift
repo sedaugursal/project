03e142d2547bdf2ba2cb06472cd30551ba50ded6
sedaugursal <seda.ugursal@gmail.com>
2017-05-26 12:53:44 +0300

    Constant

---
+++ Constant.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class Constant: NSObject {
+    
+    static let USER_INFO : String = "USER_INFO"
+    static let IS_USER_LOGIN  : String = "IS_USER_LOGIN"
+    
+    static let TABLE_NO : String = "TABLE_NO"
+}
